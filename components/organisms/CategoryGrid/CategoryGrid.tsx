2c674cbd557d428b36a91b2acd532d2b5247367d
Shashi Ranjan Prajapati <77846469+ShashiRanjanPrajapati@users.noreply.github.com>
2026-06-08 10:26:42 +0530

    pushing quiz app code

--- components/organisms/CategoryGrid/CategoryGrid.tsx
+++ components/organisms/CategoryGrid/CategoryGrid.tsx
@@ -13,7 +13,7 @@ export function CategoryGrid({
   onCategoryClick,
 }: CategoryGridProps) {
   return (
-    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {categories.map((category) => (
         <CategoryCard
           key={category.id}
